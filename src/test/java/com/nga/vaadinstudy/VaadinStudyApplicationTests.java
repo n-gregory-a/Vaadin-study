209a44dd7705e9f78ce7c38626333ec297a6bdea
Grigory <ovonely@gmail.com>
2020-02-09 19:16:47 +0700

    Vaadin studying:   - creating project

---
+++ src/test/java/com/nga/vaadinstudy/VaadinStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nga.vaadinstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaadinStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
