2ea54c63385d83854893f1ce22e5b119e4b5a78c
Grigory <ovonely@gmail.com>
2020-02-12 21:34:57 +0700

    Vaadin studying:   - button

--- src/main/java/com/nga/vaadinstudy/MainView.java
+++ src/main/java/com/nga/vaadinstudy/MainView.java
@@ -1,13 +1,18 @@
 package com.nga.vaadinstudy;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+
+import java.awt.*;
 
 @Route
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        add(new H1("Hello World!"));
+        Button button = new Button("Click me", event -> Notification.show("Clicked!"));
+        add(button);
     }
 }
